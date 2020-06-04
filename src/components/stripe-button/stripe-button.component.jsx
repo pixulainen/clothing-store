3c7f7ab4aff117c9e50c9e519594edf25dafe275
pixulainen <george.apetrei@gmail.com>
2020-06-05 00:03:01 +0100

    stripe itegration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51GqQn2Eq4mmiaBlttJMcAL8KIEEJtBYnH58gLiwYoxTvaMm0GRgWvnfm9JspWhvngfdg6IKLmzzU5Zh13yW599Oy00ZisT3vPn';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='Clothing-Company'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is ${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
