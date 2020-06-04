a1b4704ab786b0a95f4c08059e94a78da713e912
pixulainen <george.apetrei@gmail.com>
2020-06-04 22:23:06 +0100

    categories

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-
+import { persistStore } from 'redux-persist';
 import rooteReducer from './root-reducer';
+
 const middleware = [ logger ];
 
-const store = createStore(rooteReducer, applyMiddleware(...middleware));
-export default store;
+export const store = createStore(rooteReducer, applyMiddleware(...middleware));
+export const persistor = persistStore(store);
+export default { store, persistor };
